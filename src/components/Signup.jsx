4f28e3f0c631487f743e1fe27345e16a47ecf569
Ubond Ndoh <ubongndoh@gmail.com>
2023-05-14 14:19:15 +0100

    logins

---
+++ src/components/Signup.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { signupFields } from "../constants/formFields";
+import FormAction from "./FormActions";
+import Input from "./Input";
+
+const fields = signupFields;
+let fieldsState = {};
+
+fields.forEach((field) => (fieldsState[field.id] = ""));
+
+export default function Signup() {
+  const [signupState, setSignupState] = useState(fieldsState);
+
+  const handleChange = (e) =>
+    setSignupState({ ...signupState, [e.target.id]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(signupState);
+    createAccount();
+  };
+
+  //handle Signup API Integration here
+  const createAccount = () => {};
+
+  return (
+    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="">
+        {fields.map((field) => (
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={signupState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+            placeholder={field.placeholder}
+          />
+        ))}
+        <div className="flex items-center">
+          <input
+            id="remember-me"
+            name="remember-me"
+            type="checkbox"
+            className="h-4 w-4 text-purple-600 focus:ring-purple-500 border-gray-300 rounded"
+          />
+          <label
+            htmlFor="remember-me"
+            className="ml-2 block text-sm text-gray-900"
+          >
+            By Signing up, you agree to our Terms and Conditions
+          </label>
+        </div>
+        <FormAction handleSubmit={handleSubmit} text="Signup" />
+      </div>
+    </form>
+  );
+}
---
+++ src/constants/formFields.js
@@ -0,0 +1,67 @@
+const loginFields = [
+  {
+    labelText: "Email address",
+    labelFor: "email-address",
+    id: "email-address",
+    name: "email",
+    type: "email",
+    autoComplete: "email",
+    isRequired: true,
+    placeholder: "Email address",
+  },
+  {
+    labelText: "Password",
+    labelFor: "password",
+    id: "password",
+    name: "password",
+    type: "password",
+    autoComplete: "current-password",
+    isRequired: true,
+    placeholder: "Password",
+  },
+];
+
+const signupFields = [
+  {
+    labelText: "Full Name",
+    labelFor: "FullName",
+    id: "FullName",
+    name: "FullName",
+    type: "text",
+    autoComplete: "FullName",
+    isRequired: true,
+    placeholder: "Full Name",
+  },
+  {
+    labelText: "Email address",
+    labelFor: "email-address",
+    id: "email-address",
+    name: "email",
+    type: "email",
+    autoComplete: "email",
+    isRequired: true,
+    placeholder: "Email address",
+  },
+  {
+    labelText: "Password",
+    labelFor: "password",
+    id: "password",
+    name: "password",
+    type: "password",
+    autoComplete: "current-password",
+    isRequired: true,
+    placeholder: "Password",
+  },
+  {
+    labelText: "Confirm Password",
+    labelFor: "confirm-password",
+    id: "confirm-password",
+    name: "confirm-password",
+    type: "password",
+    autoComplete: "confirm-password",
+    isRequired: true,
+    placeholder: "Confirm Password",
+  },
+];
+
+export { loginFields, signupFields };
